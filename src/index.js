f3d29d50899cda1c44eb081edbb7b24b79a37d90
Prodiler <triplasm@yandex.ru>
2019-06-17 17:04:35 +0300

    Posting changes to the server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
 const routes = require("./routes");
-var bodyParser = require('body-parser').urlencoded({ extended : true });
 
 app.set('view engine', 'ejs');
 
 app.use('/public', express.static('public'));
-app.use(bodyParser);
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use('/', routes);
 
 app.listen(3000, function(err) {
--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,52 @@
 const express = require('express');
 let routes = express.Router();
+var user_info = {};
 
 routes.get('/', function(req, res, next) {
-    res.render('index');
+  res.render('index');
 });
 
 routes.get('/index', function(req, res, next) {
-    res.render('index');
+  res.render('index');
 });
 
 routes.get('/language_level', function(req, res, next) {
-    res.render('language_level');
+  res.render('language_level');
+});
+routes.post('/language_level', function(req, res) {
+  if(!req.body) console.log("ERROR POST");
+  user_info.user_name = req.body.userName;
+  res.render('language_level');
 });
 
+
 routes.get('/memory_type', function(req, res, next) {
-    res.render('memory_type');
+  res.render('memory_type');
+});
+routes.post('/memory_type', function(req, res) {
+  if(!req.body) console.log("ERROR POST");
+  user_info.lang_level = req.body.level;
+  res.render('memory_type');
 });
 
 routes.get('/data_input', function(req, res, next) {
-    res.render('data_input');
+  res.render('data_input');
+});
+routes.post('/data_input', function(req, res) {
+  if(!req.body) console.log("ERROR POST");
+  user_info.mem_type = req.body.memory;
+  res.render('data_input');
 });
 
+
 routes.get('/demo1', function(req, res, next) {
-    res.render('demo1');
+  res.render('demo1');
+});
+routes.post('/demo1', function(req, res) {
+  if(!req.body) console.log("ERROR POST");
+  user_info.dat_input = req.body.input;
+  //console.log(user_info); // Оставил для контроля
+  res.render('demo1');
 });
 
 routes.get('*', function(req, res, next) {
